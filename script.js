624bbb7c089ea9bf7c125d8e472c87c1d34e2d81
akmanni1 <142551270+akmanni1@users.noreply.github.com>
2024-10-16 22:31:00 -0400

    Search conditions updated

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function validateForm(event){
   let errors = "<ul>";
   let isValid = true;
   
-  // validation of inputs
+  // Validation of form inputs
   if(!(emailRegex.test(email.value))){
     // Changes boolean flag because the form is not valid
     isValid = false;
@@ -99,6 +99,95 @@ function validateForm(event){
       firstName.nextElementSibling.classList.add("hidden");
       email.nextElementSibling.classList.add("hidden");
       phone.nextElementSibling.classList.add("hidden");
+    }
+}
+
+// Search form conditions
+
+var geocoder;
+var waitingForLocation = false;
+$(document).ready(function(){
+    geocoder=new google.maps.Geocoder();
+    $('#address').on('change textInput input',function(){
+        $('#search-lat').val('');
+        $('#search-lng').val('');
+    });
+    $('#form').on('submit',function(e){
+        if(!doSearch()){
+            e.preventDefault();initiateFindingLocation();
+    }});
+});
+
+function doSearch(){
+    let lat=$('#search-lat').val();
+    let lng=$('#search-lng').val();
+    if(lat.length<=0||lng.length<=0)return false;
+    $('#search-menu').remove();
+    $('#search-cf').remove();
+    $('#search-dedicated').remove();
+    $('#search-local').remove();
+    let chainsOption=$('#chains-option').val();
+    let gfOption=$('#gf-option').val();
+    let maxDistanceEl=$('#max-distance');
+    let maxDistance=maxDistanceEl.length?maxDistanceEl.val():'';
+    let menu=gfOption=='menu';
+    let celiacFriendly=gfOption=='cf';
+    let dedicated=gfOption=='dedicated';
+    let local=chainsOption=='local';
+    let form=$('#form');
+    if(menu){
+        $('<input>').attr({type:'hidden',id:'search-menu',name:'menu',value:'t'}).appendTo(form);
+    }
+    if(celiacFriendly){
+        $('<input>').attr({type:'hidden',id:'search-cf',name:'cf',value:'t'}).appendTo(form);
+    }
+    if(dedicated){
+        $('<input>').attr({type:'hidden',id:'search-dedicated',name:'dedicated',value:'t'}).appendTo(form);
+    }
+    if(local){
+        $('<input>').attr({type:'hidden',id:'search-local',name:'local',value:'t'}).appendTo(form);
+    }
+    if(maxDistance.length>0){
+        $('<input>').attr({type:'hidden',id:'search-md',name:'md',value:maxDistance}).appendTo(form);
+    }
+    return true;
+}
+
+function initiateFindingLocation(){
+    let loader=$('<div></div>').addClass('loader');
+    $('#loader-container').append(loader);
+    let address=$('#address').val();
+    if(address.length>0){
+        geocoder.geocode({'address':address},function(results,status){
+            if(status==google.maps.GeocoderStatus.OK){
+                var result=results[0];
+                var loc=result.geometry.location;
+                $('#search-address').val(result.formatted_address);
+                updateLatLngAndSearch(loc.lat(),loc.lng());
+                return false;
+            }else{
+                $('#loader-container').html('');alert("Unable to find: "+address);
+            }});
+        }else{
+            if(navigator.geolocation){
+                waitingForLocation=true;
+                navigator.geolocation.getCurrentPosition(advancedPositionUpdate,advancedPositionError);
+            }else{$('#loader-container').html('');
+            alert('Current location is not supported. Please enter a city or address.');}
+    }
+}
+function updateLatLngAndSearch(lat,lng){
+    $('#search-lat').val(lat);$('#search-lng').val(lng);$('#form').submit();
+}
+function advancedPositionError(error){
+    $('#loader-container').html('');console.log(error);
+    alert("Unable to find current location. Please enter a city or address.");
+}
+function advancedPositionUpdate(position){
+    if(waitingForLocation){
+        waitingForLocation=false;
+        let lat=position.coords.latitude;
+        let lng=position.coords.longitude;updateLatLngAndSearch(lat,lng);
     }
 }
 
